d0111e65ab7e26c031a91480bf66098bf232c1ce
Caroline Granlund <granlundcaroline@gmail.com>
2018-10-21 00:28:31 +0200

    Added fonts

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
   render() {
     return (
       <nav className="footer nav justify-content-center">
-        <div className="Footer-text">
+        <div className="Footer-text portfolio-text">
           <h6>Caroline Granlund</h6>
           <p><small>
             <a href="mailto:granlundcaroline@gmail.com">granlundcaroline@gmail.com</a>
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -10,15 +10,15 @@ class LandingPage extends Component {
           <div className="col-md-3 col-sm-12 profile-image-container order-md-0 order-sm-2">
             <img className="rounded img-fluid" src="images/carrolita-small.jpg" alt="Caroline Granlund"/>
             <p><small>
-              <a className="text-muted" href="mailto:granlundcaroline@gmail.com">granlundcaroline@gmail.com</a>
+              <a className="text-muted portfolio-text" href="mailto:granlundcaroline@gmail.com">granlundcaroline@gmail.com</a>
               <br/>
-              <a className="text-muted" href="tel:+46738350555">+4673-835 05 55</a>
+              <a className="text-muted portfolio-text" href="tel:+46738350555">+4673-835 05 55</a>
             </small></p>
           </div>
           <div className="col-md-9 col-sm-12">
-            <h5 className="text-muted">Caroline Granlund</h5>
-            <h1 className="display-4">Fronted Developer</h1>
-            <p className="text-muted">Det är ett välkänt faktum att läsare distraheras av läsbar text på en sida när man skall studera layouten. Poängen med Lorem Ipsum är att det ger ett normalt ordflöde, till skillnad från "Text här, Text här", och ger intryck av att vara läsbar text.</p>
+            <h5 className="text-muted portfolio-text">Caroline Granlund</h5>
+            <h1 className="display-4 portfolio-heading">Fronted Developer</h1>
+            <p className="text-muted portfolio-text">Det är ett välkänt faktum att läsare distraheras av läsbar text på en sida när man skall studera layouten. Poängen med Lorem Ipsum är att det ger ett normalt ordflöde, till skillnad från "Text här, Text här", och ger intryck av att vara läsbar text.</p>
             <div>
               <a target="_blank" className="btn portfolio-btn" href="https://github.com/carrolita">See my Github</a>
             </div>
@@ -26,10 +26,10 @@ class LandingPage extends Component {
         </div>
 
 
-        <h1 className="mt-5">Tech</h1>
+        <h1 className="mt-5 portfolio-heading">Tech</h1>
         <div className="row justify-content-center">
           <div className="col">
-            content goes here...
+            <h4 className="text-muted portfolio-text">480 coding hours • 50+ individual tasks • HTML5, CSS, SASS, Flexbox, Javascript, ES6, JSX, React • 6 Sprints with demos together with companies like Volumental, Boniers and Comprend</h4>
           </div>
         </div>
 
@@ -39,7 +39,7 @@ class LandingPage extends Component {
               <div className="col-md-4 col-sm-12 mt-3">
                 <div className="card">
                   <div className="card-body">
-                    <p className="card-text"><small className="text-muted">{item.description}</small></p>
+                    <p className="card-text"><small className="text-muted portfolio-text">{item.description}</small></p>
                   </div>
                   <img className="card-img-bottom" src={item.image} alt={item.description} />
                 </div>
@@ -50,23 +50,42 @@ class LandingPage extends Component {
 
         <div className="row mt-5">
           <div className="col-md-4 col-sm-12 mt-3">
-            <h6 className="text-muted">Code</h6>
+            <h6 className="text-muted portfolio-heading">Code</h6>
+            <p className="text-muted portfolio-text">
+              HTML5<br/>
+              CSS3<br/>
+              JavaScript ES6<br/>
+              React<br/>
+              Github<br/>
+            </p>
           </div>
           <div className="col-md-4 col-sm-12 mt-3">
-            <h6 className="text-muted">Toolbox</h6>
+            <h6 className="text-muted portfolio-heading">Toolbox</h6>
+            <p className="text-muted portfolio-text">
+              Atom<br/>
+              Postman<br/>
+              Slack<br/>
+            </p>
           </div>
           <div className="col-md-4 col-sm-12 mt-3">
-            <h6 className="text-muted">More</h6>
+            <h6 className="text-muted portfolio-heading">More</h6>
+            <p className="text-muted portfolio-text">
+              Agile methodology<br/>
+            </p>
           </div>
           <div className="col-md-4 col-sm-12 mt-3">
-            <h6 className="text-muted">Upcoming</h6>
+            <h6 className="text-muted portfolio-heading">Upcoming</h6>
+            <p className="text-muted portfolio-text">
+              Redux<br/>
+            </p>
           </div>
         </div>
 
-        <h1 className="mt-5">Studies</h1>
+        <h1 className="mt-5 portfolio-heading">Studies</h1>
         <div className="row justify-content-center">
           <div className="col">
-            <h4 className="text-muted">Frontend Developer</h4>
+            <h4 className="text-muted portfolio-heading">Frontend Developer</h4>
+              <p className="text-muted portfolio-text">Full time 12 week bootcamp</p>
           </div>
         </div>
 
