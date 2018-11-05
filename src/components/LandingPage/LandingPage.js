f52c27c37b4117d1208c0d12046cc7c4440a60ce
Caroline Granlund <granlundcaroline@gmail.com>
2018-11-05 23:35:03 +0100

    done

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -17,16 +17,26 @@ class LandingPage extends Component {
           </div>
           <div className="col-md-9 col-sm-12">
             <h5 className="text-muted portfolio-text">Caroline Granlund</h5>
-            <h1 className="display-4 portfolio-heading">Fronted Developer</h1>
+            <h1 className="display-4 portfolio-heading">Frontend Developer</h1>
             <p className="text-muted portfolio-text">Hi,<br/>
               My name is Caroline and I have been working as a
-              Pre-school teacher for many years. Passing the years
+              Pre-school teacher for many years. Over the years
               I got interested in programming because I got inspired
-              from family members who works with programming.
-              They challenged my to try it on and I did. Now during
-              The Bootcamp I really like it. With programming you
-              learn new stuff all the time and it’s really really fun!
-               </p>
+              from my family members who works with programming. I started
+              to play Code Combat and later I started to write some
+              lines of code.
+             </p>
+             <p className="text-muted portfolio-text">
+                Everything started with HTML and CSS. Adding
+                text and colors was like magic for me. Anyways,
+                I got challenged to try it, and I did. During
+                the bootcamp I really grew to like it! With programming you can
+                learn new stuff all the time and it’s really really fun!
+                From pre-school teacher to frontend developer, crazy huh?
+                I also have a lot of experience of working in groups with
+                differente kinds of people and in both stressful and calm situations.
+                Try me!
+             </p>
             <div>
               <a target="_blank" className="btn portfolio-btn" href="https://github.com/carrolita">See my Github</a>
             </div>
@@ -37,7 +47,7 @@ class LandingPage extends Component {
         <h1 className="mt-5 portfolio-heading">Tech</h1>
         <div className="row justify-content-center">
           <div className="col">
-            <h4 className="text-muted portfolio-text">480 coding hours • 50+ individual tasks • HTML5, CSS, SASS, Flexbox, Javascript, ES6, JSX, React • 6 Sprints with demos together with companies like Volumental, Boniers and Comprend</h4>
+            <h4 className="text-muted portfolio-text">480 coding hours • 50+ individual tasks • HTML5, CSS, SASS, Flexbox, Javascript, ES6, JSX, React • 6 Sprints with demos together with companies like Volumental, Bonniers and Comprend</h4>
           </div>
         </div>
 
@@ -90,22 +100,36 @@ class LandingPage extends Component {
           </div>
         </div>
 
-        <h1 className="mt-5 portfolio-heading">Studies</h1>
-        <div className="row justify-content-center">
-          <div className="col">
+        <h1 className="mt-5 portfolio-heading text-left">Studies</h1>
+        <div className="row justify-content-left mt-3">
+          <div className="col-4">
             <h4 className="text-muted portfolio-heading">Frontend Developer</h4>
               <p className="text-muted portfolio-text text-center text-md-left">Full time 12 week bootcamp / <br />
-            intenstiv program. Covering <br/>
-            everything from basic of <br/>
-            programming to how to <br/>
-            structure and build bigger web <br/>
-            projects. Focuse on JavaScript. <br/>
-            Total 480 coding hours, 50 + <br/>
-            individual tasks (in HTML, CSS, <br/>
-            JavaScript, JSX, Node.js) <br/>
-            and 6 sprints with companies like <br/>
-            Svenska spel <br/>
-            Sthlm Sep-Nov 2018</p>
+                intenstiv program. Covering
+                everything from basic of
+                programming to how to
+                structure and build bigger web
+                projects. Focuse on JavaScript.
+                Total 480 coding hours, 50 +
+                individual tasks (in HTML, CSS,
+                JavaScript, JSX, Node.js)
+                and 6 sprints with companies like
+                Svenska spel and Fyndiq
+                Sthlm Sep-Nov 2018</p>
+          </div>
+          <div className="col-4">
+            <h4 className="text-muted portfolio-heading">Banyan Center</h4>
+              <p className="text-muted portfolio-text text-center text-md-left">2 years austism work/ educaction <br />
+                BanyanCenter is a psychologist's reception for small children with autism spectrum diagnosis. BanyanCenter works with early intensive behavioral interventions (eibi) for children with autism. I worked with children with autism for 2 years.
+                Sthlm 2014-2016</p>
+          </div>
+          <div className="col-4">
+            <h4 className="text-muted portfolio-heading">Vackstanas gymnasiet</h4>
+              <p className="text-muted portfolio-text text-center text-md-left">3 years at (gymnasiet) <br />
+                 I learned Textile Shape and Design. We created our own clothes collection
+                 and had fashion shows.
+                  Sthlm, 2008-2011.</p>
+
           </div>
         </div>
 
